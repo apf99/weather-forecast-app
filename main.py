2c75096d4113c0e834e61a2a3a2931546b2624c5
Andrew Friedman <andrew@cyberdreaming.net>
2024-08-28 06:36:51 -0700

    Some refactoring of the code plus agged images for the sky conditions

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import streamlit as st
 import plotly.express as px
 from backend import get_data
 
+
 st.title('Weather Forecast for the Next Days')
 place = st.text_input('Place: ')
 days = st.slider('Forecast Days', min_value=1, max_value=5,
@@ -10,9 +11,17 @@ option = st.selectbox('Select data to view',
                       ('Temperature', 'Sky'))
 st.subheader(f'{option} for the next {days} days in {place}')
 
-days, temperatures = get_data(place, days, option)
-
-labels = {'x': 'Dates', 'y': 'Temperature (C)'}
-figure = px.line(x=dates, y=temperatures, labels=labels)
-st.plotly_chart(figure)
+if place:
+    # get the data
+    data = get_data(place, days)
+    if option == 'Temperature':
+        temperatures = [item['main']['temp'] for item in data]
+        dates = [item['dt_txt'] for item in data]
+        labels = {'x': 'Dates', 'y': 'Temperature (C)'}
+        figure = px.line(x=dates, y=temperatures, labels=labels)
+        st.plotly_chart(figure)
+    elif option == 'Sky':
+        sky_conditions = [item['weather'][0]['main'] for item in data]
+        paths = [f'images/{condition.lower()}.png' for condition in sky_conditions]
+        st.image(paths, width=115)
 
